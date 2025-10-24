bce9e04f3c77ca32020860d5856998e8b8d0544e
Arijit <arijit.biswas@bombaysoftwares.com>
2025-10-24 23:45:02 +0530

    pdf generation pace survey

--- src/ParkSpaceHub/settings.py
+++ src/ParkSpaceHub/settings.py
@@ -82,6 +82,30 @@ DATABASES = {
     }
 }
 
+# Ensure logs folder exists
+logs_dir = os.path.join(BASE_DIR, 'logs')
+if not os.path.exists(logs_dir):
+    os.makedirs(logs_dir)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(logs_dir, 'parkhub.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
